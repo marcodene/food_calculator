234c5617ad0ca4498d20972a3b67fe9fd18f4f1b
marcodene <mdenegri03@gmail.com>
2021-03-30 15:47:54 +0200

    added public folder and made it as a static directory

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 require("./db/mongoose");
 const userRouter = require("./router/user");
 const foodRouter = require("./router/food");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 app.use("/api", userRouter);
 app.use("/api", foodRouter);
